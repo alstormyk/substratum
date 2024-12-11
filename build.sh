80c49ba529de84c4844678398ef8e5d84f34e9e1
Arashi <29407286+alstormyk@users.noreply.github.com>
2024-12-11 13:26:29 +0100

    Add: some fonts

--- build.sh
+++ build.sh
@@ -11,9 +11,10 @@ RELEASE="$(rpm -E %fedora)"
 # RPMfusion repos are available by default in ublue main images
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
-
+curl -Lo /etc/yum.repos.d/atim-starship-fedora-"${RELEASE}".repo https://copr.fedorainfracloud.org/coprs/atim/starship/repo/fedora-"${RELEASE}"/atim-starship-fedora-"${RELEASE}".repo
+curl -Lo /etc/yum.repos.d/che-nerd-fonts-fedora-"${RELEASE}".repo https://copr.fedorainfracloud.org/coprs/che/nerd-fonts/repo/fedora-"${RELEASE}"/che-nerd-fonts-fedora-"${RELEASE}".repo
 # this installs a package from fedora repos
-rpm-ostree install aerc alacritty helix pass wayfire
+rpm-ostree install aerc alacritty emacs helix iosevka jetbrains-mono-fonts nerd-fonts pass wayfire
 
 # this would install a package from rpmfusion
 # rpm-ostree install vlc
