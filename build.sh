5a51fd9c4d290e6e2b40b0213c0d05c4a5e14785
Arashi <29407286+alstormyk@users.noreply.github.com>
2024-12-11 14:31:08 +0100

    Remove: peterwu/rendezvous

--- build.sh
+++ build.sh
@@ -13,9 +13,9 @@ RELEASE="$(rpm -E %fedora)"
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 curl -Lo /etc/yum.repos.d/atim-starship-fedora-"${RELEASE}".repo https://copr.fedorainfracloud.org/coprs/atim/starship/repo/fedora-"${RELEASE}"/atim-starship-fedora-"${RELEASE}".repo
 curl -Lo /etc/yum.repos.d/che-nerd-fonts-fedora-"${RELEASE}".repo https://copr.fedorainfracloud.org/coprs/che/nerd-fonts/repo/fedora-"${RELEASE}"/che-nerd-fonts-fedora-"${RELEASE}".repo
-curl -Lo /etc/yum.repos.d/peterwu-rendezvous-fedora-"${RELEASE}".repo https://copr.fedorainfracloud.org/coprs/peterwu/rendezvous/repo/fedora="${RELEASE}"/peterwu-rendezvous-fedora-"${RELEASE}".repo
+#curl -Lo /etc/yum.repos.d/peterwu-rendezvous-fedora-"${RELEASE}".repo https://copr.fedorainfracloud.org/coprs/peterwu/rendezvous/repo/fedora="${RELEASE}"/peterwu-rendezvous-fedora-"${RELEASE}".repo
 # this installs a package from fedora repos
-rpm-ostree install aerc alacritty emacs helix iosevka-fusion-fonts isync jetbrains-mono-fonts nerd-fonts notmuch pass wayfire
+rpm-ostree install aerc alacritty emacs helix isync jetbrains-mono-fonts nerd-fonts notmuch pass wayfire
 
 # this would install a package from rpmfusion
 # rpm-ostree install vlc
