b6342ce1ef102e303dba33f32072477de7f89186
Arashi <29407286+alstormyk@users.noreply.github.com>
2024-12-11 13:51:08 +0100

    Add: ioseveka font & isync

--- build.sh
+++ build.sh
@@ -13,8 +13,9 @@ RELEASE="$(rpm -E %fedora)"
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 curl -Lo /etc/yum.repos.d/atim-starship-fedora-"${RELEASE}".repo https://copr.fedorainfracloud.org/coprs/atim/starship/repo/fedora-"${RELEASE}"/atim-starship-fedora-"${RELEASE}".repo
 curl -Lo /etc/yum.repos.d/che-nerd-fonts-fedora-"${RELEASE}".repo https://copr.fedorainfracloud.org/coprs/che/nerd-fonts/repo/fedora-"${RELEASE}"/che-nerd-fonts-fedora-"${RELEASE}".repo
+curl -Lo /etc/yum.repos.d/peterwu-iosveka-fedora-"${RELEASE}".repo https://copr.fedorainfracloud.org/coprs/peterwu/iosveka/repo/fedora="${RELEASE}"/peterwu-iosveka-fedora-"${RELEASE}".repo
 # this installs a package from fedora repos
-rpm-ostree install aerc alacritty emacs helix iosevka jetbrains-mono-fonts nerd-fonts pass wayfire
+rpm-ostree install aerc alacritty emacs helix iosevka isync jetbrains-mono-fonts nerd-fonts pass wayfire
 
 # this would install a package from rpmfusion
 # rpm-ostree install vlc
