a9bb5ca928ae65da2308c7f593c55d0a47e8354f
Amy Storm Kosman <29407286+alstormyk@users.noreply.github.com>
2024-12-11 21:26:14 +0000

    Add: sway

--- build.sh
+++ build.sh
@@ -11,11 +11,13 @@ RELEASE="$(rpm -E %fedora)"
 # RPMfusion repos are available by default in ublue main images
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
-curl -Lo /etc/yum.repos.d/atim-starship-fedora-"${RELEASE}".repo https://copr.fedorainfracloud.org/coprs/atim/starship/repo/fedora-"${RELEASE}"/atim-starship-fedora-"${RELEASE}".repo
 curl -Lo /etc/yum.repos.d/che-nerd-fonts-fedora-"${RELEASE}".repo https://copr.fedorainfracloud.org/coprs/che/nerd-fonts/repo/fedora-"${RELEASE}"/che-nerd-fonts-fedora-"${RELEASE}".repo
-#curl -Lo /etc/yum.repos.d/peterwu-rendezvous-fedora-"${RELEASE}".repo https://copr.fedorainfracloud.org/coprs/peterwu/rendezvous/repo/fedora="${RELEASE}"/peterwu-rendezvous-fedora-"${RELEASE}".repo
+curl -Lo /etc/yum.repos.d/peterwu-rendezvous-fedora-"${RELEASE}".repo https://copr.fedorainfracloud.org/coprs/peterwu/rendezvous/repo/fedora-"${RELEASE}"/peterwu-rendezvous-fedora-"${RELEASE}".repo
 # this installs a package from fedora repos
-rpm-ostree install aerc alacritty emacs helix isync jetbrains-mono-fonts nerd-fonts notmuch pass wayfire
+rpm-ostree install alacritty dunst fzf helix jetbrains-mono-fonts kanshi mako nerd-fonts \
+                   rofi-wayland sddm sddm-wayland-sway \
+                   sway sway-bg sway-config swaylock sway-systemd thunar \
+                   waybar wayfire wf-shell wlr-randr wlsunset wofi
 
 # this would install a package from rpmfusion
 # rpm-ostree install vlc
